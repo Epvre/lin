3b8d858ba421e0838440c01ce40cac068bd67382
KlyuchNK <43313986+KlyuchNK@users.noreply.github.com>
2023-10-27 21:15:16 +0700

    preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+#Check Sign of Distributive
+VER=cat()
+
+#Mounting USB
+fdisk -l
+echo "Write Device name for your USB"
+read USB
+mkdir /mnt/usb
+mount /dev/$USB /mnt/usb
+
 #Try resolve Erroring about coredump
 sysctl -w fs.suid_dumpable=0
+
 
 
