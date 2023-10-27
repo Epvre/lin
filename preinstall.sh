3b1a4747a1b164bc96c703e90c308dc8f7085cf4
KlyuchNK <43313986+KlyuchNK@users.noreply.github.com>
2023-10-27 21:25:43 +0700

    preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
+FILENAME=$BASH_SOURCE
 
-#Check Sign of Distributive
-VER=cat()
-
-#Mounting USB
+#Mounting USB for reading version
 fdisk -l
 echo "Write Device name for your USB"
 read USB
 mkdir /mnt/usb
 mount /dev/$USB /mnt/usb
 
+#Check Sign of Distributive
+VER=cat /mnt/usb/arch/version
+
 #Try resolve Erroring about coredump
 sysctl -w fs.suid_dumpable=0
 
 
+
+
+#Activating this file 
+chmod +x $FILENAME
 
