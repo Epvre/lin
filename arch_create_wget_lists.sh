28f9bb5fa56c2cf32a67524da2fedf573d577281
Epvre <43313986+Epvre@users.noreply.github.com>
2024-03-04 16:30:12 +0700

    Update arch_create_wget_lists.sh

--- arch_create_wget_lists.sh
+++ arch_create_wget_lists.sh
@@ -6,7 +6,7 @@ URL_DIR=https://archive.archlinux.org/repos/2024/03/01/core/os/x86_64/
 #curl -s $URL_DIR | grep -oE '[^/]+\.pkg\.tar\.zst' > $IN
 >$OUT
 
-# Цикл для обработки каждой строки в списке
+# For each line cycle
 while read -r LINE; do
     # Получить название файла (первое слово в строке)
     FILENAME=$(echo $LINE | awk '{print $1}')
@@ -21,15 +21,17 @@ done < $IN
 #EXTRA
 IN=list_arch_extra_20240301.txt
 OUT=wget_list_arch_extra.txt
+URL_DIR=https://archive.archlinux.org/repos/2024/03/01/extra/os/x86_64/
+#curl -s $URL_DIR | grep -oE '[^/]+\.pkg\.tar\.zst' > $IN
 >$OUT
 
-# Цикл для обработки каждой строки в списке
+# For each line cycle
 while read -r LINE; do
     # Получить название файла (первое слово в строке)
     FILENAME=$(echo $LINE | awk '{print $1}')
     
     # Добавить ссылку спереди
-    URL="https://archive.archlinux.org/repos/2024/03/01/extra/os/x86_64/$FILENAME"
+    URL="$URL_DIR/$FILENAME"
     
     # Сохранить результат в файл
     echo $URL >> $OUT
