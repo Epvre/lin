7955709e242837f7dcca820a6501810bbb73cc60
Epvre <43313986+Epvre@users.noreply.github.com>
2024-03-04 19:20:39 +0700

    Update arch_create_wget_lists.sh

--- arch_create_wget_lists.sh
+++ arch_create_wget_lists.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
-#CORE OR EXTRA
+#Mount and folder creation
+MNT=/mnt/sdb1
+DATE=2024/03/01
 TYPE=extra
+
+REPO_DIR=$MNT_DIR/repos/$DATE/$TYPE/os/x86_64/
+
+mkdir -p $REPO_DIR
+
+#IN and OUT creation
+
 IN=list_arch_$TYPE_20240301.txt
 OUT=wget_list_arch_$TYPE.txt
-URL_DIR="https://archive.archlinux.org/repos/2024/03/01/$TYPE/os/x86_64/"
+
+URL_DIR="https://archive.archlinux.org/repos/$DATE/$TYPE/os/x86_64/"
 
 >$OUT
+
+#Extract links from http
 curl -s $URL_DIR | grep -o 'href="[^"]\+"' | sed 's/href="\([^"]\+\)"/\1/g' > $IN
 
 
-
-# For each line cycle
+# Create wget-list
 while read -r LINE; do
     FILENAME=$(echo $LINE | awk '{print $1}')
     URL="$URL_DIR/$FILENAME"
     echo $URL >> $OUT
 done < $IN
+
+#Downloading by wget-list
+wget --input-file=$OUT --continue -o $REPO_DIR
 
  
