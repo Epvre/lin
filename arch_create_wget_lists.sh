a42efbb35b5dd589cd890d15087dcdb1eefea61d
Epvre <43313986+Epvre@users.noreply.github.com>
2024-03-04 20:48:57 +0700

    Update arch_create_wget_lists.sh

--- arch_create_wget_lists.sh
+++ arch_create_wget_lists.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 #Mount and folder creation
 MNT=/mnt/sdb1
-
 DATE=2024/03/01
 TYPE=extra
-
-
 REPO_DIR=$MNT_DIR/repos/$DATE/$TYPE/os/x86_64/
 
 mkdir -p $REPO_DIR
 
 
 #IN and OUT creation
-
 IN=list_arch_$TYPE_20240301.txt
 OUT=wget_list_arch_$TYPE.txt
-
 DOMAIN=https://archive.archlinux.org
 URL_DIR=$DOMAIN/repos/$DATE/$TYPE/os/x86_64/
 
@@ -26,7 +21,7 @@ pacman -S aria2
 curl -s $URL_DIR | grep -o 'href="[^"]\+"' | sed 's/href="\([^"]\+\)"/\1/g' > $IN
 
 #curl -s $URL_DIR  | grep -o 'href="[^"]\+"' | sed 's/href="\([^"]\+\)"/\1/g' | aria2c -j8 -i - -d $REPO_DIR
-wget -e robots=off --recursive --no-clobber --page-requisites --html-extension --convert-links --domains $DOMAIN --no-parent $URL_DIR
+#wget -e robots=off --recursive --no-clobber --page-requisites --html-extension --convert-links --domains $DOMAIN --no-parent $URL_DIR
 
 
 # Create wget-list
