6359a203d7c6a379d085f67bef49592f4df74438
Epvre <43313986+Epvre@users.noreply.github.com>
2024-03-04 18:50:35 +0700

    Update arch_create_wget_lists.sh

--- arch_create_wget_lists.sh
+++ arch_create_wget_lists.sh
@@ -6,20 +6,15 @@ OUT=wget_list_arch_$TYPE.txt
 URL_DIR="https://archive.archlinux.org/repos/2024/03/01/$TYPE/os/x86_64/"
 
 >$OUT
-curl -s $URL_DIR | grep -o 'href="[^"]\+"' | sed 's/href="\([^"]\+\)"/\1/g' > $OUT
+curl -s $URL_DIR | grep -o 'href="[^"]\+"' | sed 's/href="\([^"]\+\)"/\1/g' > $IN
 
 
 
 # For each line cycle
-# while read -r LINE; do
-    # Получить название файла (первое слово в строке)
-#    FILENAME=$(echo $LINE | awk '{print $1}')
-    
-    # Добавить ссылку спереди
-#    URL="$URL_DIR/$FILENAME"
-    
-    # Сохранить результат в файл
-#    echo $URL >> $OUT
-# done < $IN
+while read -r LINE; do
+    FILENAME=$(echo $LINE | awk '{print $1}')
+    URL="$URL_DIR/$FILENAME"
+    echo $URL >> $OUT
+done < $IN
 
  
