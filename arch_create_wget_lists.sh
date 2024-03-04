94da3c45687ba0f34af2bf95481cd802752aaf49
Epvre <43313986+Epvre@users.noreply.github.com>
2024-03-04 18:18:38 +0700

    Update arch_create_wget_lists.sh

--- arch_create_wget_lists.sh
+++ arch_create_wget_lists.sh
@@ -2,7 +2,7 @@
 #CORE
 IN=list_arch_core_20240301.txt
 OUT=wget_list_arch_core.txt
-URL_DIR=https://archive.archlinux.org/repos/2024/03/01/core/os/x86_64/
+URL_DIR=https://archive.archlinux.org/repos/2024/03/01/extra/os/x86_64/
 
 >$OUT
 curl -s $URL_DIR | awk '/\.pkg\.tar\.zst$/{getline url; print url}' > $OUT
